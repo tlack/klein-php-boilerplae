86218d13c742eb531007d0cf4fbcf98be1de5e19
Thomas Lackner <lackner@gmail.com>
2012-12-21 21:53:12 -0600

    flash support for headers

--- templates/global/header.php
+++ templates/global/header.php
@@ -1,3 +1,18 @@
 <html>
 <title></title>
 <body>
+	<div id="page">
+		<h1>Subly.co</h1>
+
+		<div id="content">
+		<?
+		if (isset($response)) {
+			$flashes = $response->flashes();
+			foreach ($flashes as $type=>$entries) {
+				?>
+				<div class="<?= $type; ?>">
+					<?= join('', $entries); ?>
+				</div>
+				<?
+			}
+		}
